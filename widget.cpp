0e5f662a55fd973623c93bdec714aa0f54ba082c
Maxim Selyanin <51035205+maxim-selyanin@users.noreply.github.com>
2021-05-25 13:21:57 +0300

    Update widget.cpp
    Добавил drop event, чтобы картинки можно было дропать

--- widget.cpp
+++ widget.cpp
@@ -4,21 +4,47 @@
 #include "gaussianbluralgo.h"
 #include <QDebug>
 #include "gaussfunction.h"
+#include <QMimeData>
 
 void Widget::mousePressEvent(QMouseEvent *)
 {
+    if (image.isNull()) {
+        return;
+    }
     //здесь можно изменить радиус окна
-    gb::blurImage(image, gf::getMatrix(3));
+    gb::blurImage(image, gf::getMatrix(blurMatrixRadius));
     this->setPixmap(QPixmap::fromImage(image));
+}
+
+void Widget::dragEnterEvent(QDragEnterEvent *event)
+{
+    event->accept();
+}
+
+void Widget::dragMoveEvent(QDragMoveEvent *event)
+{
+    event->accept();
+}
+
+void Widget::dropEvent(QDropEvent *event)
+{
+    QString mimeText = event->mimeData()->text();
+    if (mimeText.startsWith("file:///")
+            &&//текст является локальным адресом картинки
+            (mimeText.endsWith(".jpg") || mimeText.endsWith(".jpeg") || mimeText.endsWith(".bmp") || mimeText.endsWith(".png"))) {
+        mimeText.remove(0, 8);//удаляем "file:///"
+        QPixmap p(mimeText);
+        image = p.toImage();
+        this->setFixedSize(p.size());
+        this->setPixmap(p);
+    }
 }
 
 Widget::Widget(QWidget *parent)
     : QLabel(parent)
 {
-    //здесь можно изменить картинку
-    QPixmap p(pic1Path);
-    image = p.toImage();
-    this->setPixmap(p);
+    this->setAcceptDrops(true);
+    this->setFixedSize(500, 500);
 }
 
 Widget::~Widget()
